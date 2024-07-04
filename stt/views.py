8041b948ac64bfd9b2b07a3ea1e7e1d899506370
E0ho <leeyeongho3404@gmail.com>
2024-07-04 15:47:38 +0900

    remove: Branch 분리

--- stt/views.py
+++ stt/views.py
@@ -4,75 +4,58 @@ from rest_framework.decorators import action
 from django.shortcuts import render, HttpResponse, redirect, resolve_url
 from .models import *
 
-import urllib3
-import json
-import base64
-
-class ChatLogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chat_log
-        fields = '__all__'
-
-# STT API를 활용해 음성 정보 Text 화    
-# STT routing을 위한 view 함수 viewsets화
-class STTViewSet(viewsets.ModelViewSet):
-
-    serializer_class = ChatLogSerializer
-    queryset = Chat_log.objects.all()
-
+    import urllib3
+    import json
+    import base64
     
-    # ViewSet 클래스 내에서 사용자가 정의한 액션을 정함
-    # detail=False : URL에 객체 ID 포함 X, post 형식
-    @action(detail=False, methods=['post'])
-    def voice_stt(self, request):
-        80000
-        openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"        # 음성 인식 API 주소
-        apiKey = "d6dec6aa-41bf-48c4-9b3a-acbd97a70b3e"                         # API 키
-        language = "korean" # 언어
-        # 음성 파일
-        audioPath = "STT.wav"          
-        # 음성 파일 압축 (인코딩)
-        file = open(audioPath, "rb")
-        audioContents = base64.b64encode(file.read()).decode("utf8")
-        file.close()
-        
-        # 요청 형식 (언어, 음성)
-        requestJson = {    
-            "argument": {
-                "language_code": language,
-                "audio": audioContents
-            }
+    openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"        # 음성 인식 API 주소
+    apiKey = "d6dec6aa-41bf-48c4-9b3a-acbd97a70b3e"                         # API 키
+    language = "korean"                                                     # 언어
+    
+    audioPath = "t.wav"                                                 # 음성 파일
+    
+    # 음성 파일 압축 (인코딩)
+    file = open(audioPath, "rb")
+    audioContents = base64.b64encode(file.read()).decode("utf8")
+    file.close()
+    
+    # 요청 형식 (언어, 음성)
+    requestJson = {    
+        "argument": {
+            "language_code": language,
+            "audio": audioContents
         }
-        
-        # HttpRequest 요청 (응답)
-        http = urllib3.PoolManager()
-        response = http.request(
-            "POST",
-            openApiURL,
-            headers={"Content-Type": "application/json; charset=UTF-8","Authorization": apiKey},
-            body=json.dumps(requestJson)
-        )
-        
-        # 응답 코드 확인
-        print("[responseCode] " + str(response.status))
-        print("[responBody]")
-        print(str(response.data,"utf-8"))
-        
-        # Text 파일 복호화
-        data = json.loads(response.data.decode("utf-8", errors='ignore'))
-        #복화화된 Text 파일
-        data_text = data['return_object']['recognized']
-        print(data_text)
-        
-        # # 대화 기록 Model에 저장
-        chat_log = Chat_log(message=data_text)
-        chat_log.save()
-        
-        # 복호화된 Text 파일 Return
-        # rest_framework을 통한 resopnse
-        return Response(data_text)
-        
-        
+    }
+    
+    # HttpRequest 요청 (응답)
+    http = urllib3.PoolManager()
+    response = http.request(
+        "POST",
+        openApiURL,
+        headers={"Content-Type": "application/json; charset=UTF-8","Authorization": apiKey},
+        body=json.dumps(requestJson)
+    )
+    
+    # 응답 코드 확인
+    print("[responseCode] " + str(response.status))
+    print("[responBody]")
+    print(str(response.data,"utf-8"))
+    
+    # Text 파일 복호화
+    data = json.loads(response.data.decode("utf-8", errors='ignore'))
+    print(data['return_object']['recognized'])
+    
+    # 복호화된 Text 파일 Return
+    # return HttpResponse(data['return_object']['recognized'])
+
+    # 대화 기록 Model에 저장
+    chat_log = Chat_log()
+    chat_log.message = data['return_object']['recognized']
+    chat_log.save()
+    
+    return redirect(resolve_url('stt:analyze_sentence'))
+    
+    
     
     
 # Text 분석 질의 API를 활용하여 대화에서 필요한 정보 추출 (신고 장소, 인원, 인상착의 .... )
@@ -112,12 +95,14 @@ def analyze_sentence(request):
     )
     
     # 응답 코드 확인    
-    print("[responseCode] " + str(response.status))
-    print("[responBody]")
-    print(str(response.data,"utf-8"))
+    # print("[responseCode] " + str(response.status))
+    # print("[responBody]")
+    # print(str(response.data,"utf-8"))
     
     # GPT 응답 Text 복호화
     data = json.loads(response.data.decode("utf-8", errors='ignore'))
-
+    
+    print('화재 장소 추정 : ', data['return_object']['MRCInfo']['answer'])
+    
     # GPT 응답 Text Return
-    return HttpResponse(data['return_object']['MRCInfo']['answer']) 
+    return HttpResponse(data['return_object']['MRCInfo']['answer'])
