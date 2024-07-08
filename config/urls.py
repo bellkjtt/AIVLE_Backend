cf6d41c04dde52b4216941fecbd68c7abf38bbfd
Soo <01071236398y@gmail.com>
2024-07-08 09:57:57 +0900

    chore: urls.py 설정 변경

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from stt.views import STTViewSet
 
 # 기본 라우터 객체 생성
 router = DefaultRouter()
-# STTviewset 뷰셋을 등록
-router.register(r'stt', STTViewSet, basename='stt')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
