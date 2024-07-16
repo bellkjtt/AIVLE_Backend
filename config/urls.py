4976da05dd0c503d26e2b52fb6609b1f08d1f3a8
E0ho <leeyeongho3404@gmail.com>
2024-07-16 15:39:50 +0900

    fix: rag_gpt 앱 삭제로 인한 경로 오류 해결

--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,11 @@ from stt import views
 urlpatterns = [
     path('admin/', admin.site.urls),                   # 관리자
     path('', include('stt.urls')),
-    path('rag_gpt/', include('rag_gpt.urls')),         # RAG_GPT (긴급 구조 신고 상황 판단)
-    path('account/', include('account.urls')),         # 회원정보
-    path('api/', include("api.urls")),                 # 모델링
-    path('post/', include("post.urls")),               # 게시판
+
+    
+    # stt view 연결
+    path('account/', include('account.urls')),
+    path('api/', include("api.urls")),
+    
 ]
+
