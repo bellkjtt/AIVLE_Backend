a4b1b31c1139138281e0104e4ef91e88b2a0a13e
bellkjtt <bellkjtt@gmail.com>
2024-07-17 20:06:10 +0900

    urls.py도 작동하도록 함

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
 # api/urls.py
 from django.urls import path
-#from .views import PredictView, Disaster
-from .views import Disaster
+from .views import Disaster,PredictView
 
 urlpatterns = [
-    #path('predict/', PredictView.as_view(), name='predict'),
+    path('predict/', PredictView.as_view(), name='predict'),
     path('disaster-data/', Disaster.as_view(), name='disaster'),
-    
 ]
