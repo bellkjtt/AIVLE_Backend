c1722d0f2533c353f15f049a23027c8a7dcc2208
E0ho <leeyeongho3404@gmail.com>
2024-07-18 16:44:11 +0900

    fix: GPT 출력 결과 변경

--- modules/gpt_text_processor.py
+++ modules/gpt_text_processor.py
@@ -7,34 +7,37 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-from openai import OpenAI                           # GPT API
-from modules.estimate_address import get_address    # 신고 위치(주소, 장소) 추정
-from modules.check_duplication import check_duplication    # 신고 위치(주소, 장소) 추정
+from openai import OpenAI                                       # GPT API
+from modules.estimate_address import get_address                # 신고 위치(주소, 장소) 추정
+from modules.check_duplication import check_duplication         # 신고 위치(주소, 장소) 추정
+# from modules.classification_model import ClassificationModel    # 신고 위치(주소, 장소) 추정
+
+# classifcation = ClassificationModel()
 
 # GPT Text 정제 클래스
 class GPTProcessor:
-    
-    # 인스턴스 선언 (요구 정보, 대화 기록)
-    def __init__(self):
-        self.requirements = {'사건 분류': 'X', '사건 발생 장소': 'X', '구체적인 현장 상태': 'X'}
-        self.record = ''
 
+
+    # 인스턴스 선언 (OPENAI_API_KEY)
+    def __init__(self):
+        self.api_key = os.getenv("OPENAI_API_KEY")    # GPT API key
+        self.record = ''                              # 새로운 신고 전화
+        self.requirements = {'사건 분류': 'X', '사건 발생 장소': 'X', '구체적인 현장 상태': 'X'}
+        
 
     # 데이터 정제를 위한 전처리
     def text_preprocessor(self, sentence):
-        
-        # GPT API key
-        api_key = os.getenv("OPENAI_API_KEY")
-
-        # 신고자 음성 정보 확인
-        print('신고 음성 :', sentence)
 
         # 전체 대화 기록
-        self.record += sentence
+        self.record += '\n' + sentence
+        
+        # 신고자가 제공한 현재까지 음성 정보
+        print()
+        print('[신고 음성 기록]', self.record)
 
         # GPT 명령 하달
         command = self.record + "위 긴급 구조 신고 전화 내용을 분석하여, '사건 분류', '사건 발생 장소', '구체적인 현장 상태'에 관해 각각 답변해줘. 만약 특정 답변을 생성하기에 정보가 부족하다면, 해당 정보는 X를 답변해줘. 예를들어 '여기 불났어요' 라는 신고가 발생하면, 사건 분류 : 화재, 사건 발생 장소 : X, 구체적인 현장 상태 : X 를 리턴해주면돼."
-        client = OpenAI(api_key=api_key)
+        client = OpenAI(api_key=self.api_key)
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -60,7 +63,8 @@ class GPTProcessor:
             
             except:
                 print('GPT API 오작동 (다시 한번 말씀해주세요)')
-                return None
+                return 'GPT API 오작동 (다시 한번 말씀해주세요)'
+
 
         ################# Debugging Test ################
         # print()
@@ -69,6 +73,7 @@ class GPTProcessor:
         # print('미확인된 정보 : ', unknown_li)
         ################# Debugging Test ################
 
+        
         # 모든 필수 정보를 알아냈다.
         if not unknown_li:
             category = self.requirements['사건 분류']
@@ -96,38 +101,44 @@ class GPTProcessor:
             
             # 중복이 아닌 신고
             if not is_duplicate:
-                print('신고 접수되었습니다.')
+                print('신고가 접수되었습니다.')
+                
+                
+                # a = classifcation.classificate(self.record)
+                
+                # print(a)
+                print()
+                print('--------------------------------------------------')
+                print('[신고 내용 정제]')
                 print(context)
-                return context
+                return '신고가 접수되었습니다.'
             
             # 중복 신고
             else:
-                print('중복된 신고입니다.')
-                print(context)
-                return
+                print('이미 접수된 신고입니다.')
+                # print(context)
+                return '이미 접수된 신고입니다.'
         
         # 더 많은 정보가 필요함
         else:
-            print('정보 부족 :', unknown_li)
+            print()
+            print('신고 접수를 위해 추가로 필요한 정보 :', unknown_li)
+           
             return unknown_li
 
 
 
 
-# 모듈 단독 Test
+# 단독 모듈 Test
 if __name__ == "__main__":
     processor = GPTProcessor()
     
     # 장소 정보 제공 X
-    print('1번째 대화')
-    processor.text_preprocessor('여기 엄청 큰 불이 났어요. 연기가 너무 많이 나고 한명이 심정지 상태로 위독해보여요')
+    processor.text_preprocessor('사람이 칼에 맞아 쓰려졌어요. 피가 너무 많이 나요')
     print()
 
     print('-' * 50)
     
     # 이후에 장소 정보 제공
-    print('2번째 대화')
-    processor.text_preprocessor('여기는 서울역 스타벅스에요.')
+    processor.text_preprocessor('여기는 정자역 빽다방이에요.')
     print()
-
-    
