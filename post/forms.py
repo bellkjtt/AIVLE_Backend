8724fe285d798d28a2a33b3457c243468597f618
Sooyong Yoon <ybh6703@naver.com>
2024-07-19 14:08:06 +0900

    fix: 공지 사항 오류 수정

--- post/forms.py
+++ post/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class FileUploadForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'file', 'content']
+        fields = ['id', 'user_id', 'title', 'file', 'content']
--- post/urls.py
+++ post/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
    # path('signup/', .as_view()), # 
    path('send/',send ), # 
    path('postlist/', PostList.as_view()), # 전체 공지사항 가져오기
-   path('postdetail/', PostDetailView.as_view()), # 공지사항 하나 가져오기
+   path('postdetail/<int:pk>/', PostDetailView.as_view()), # 공지사항 하나 가져오기
    path('postcreate/', PostCreateView.as_view()), # 공지사항 생성
-   path('postedit/', PostEditView.as_view()), # 공지사항 수정
-   path('postdelete/', PostDeleteView.as_view()), # 공지사항 삭제
+   path('postedit/<int:pk>/', PostEditView.as_view()), # 공지사항 수정
+   path('postdelete/<int:pk>/', PostDeleteView.as_view()), # 공지사항 삭제
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
